7f6d3397d0e8895547c1a362e5bee8c0b462cfd1
devbigeazi <adesholatajudeen1@gmail.com>
2024-02-06 10:15:41 +0100

    Jotting some note

--- ether-simple-storage/SimpleStorage.sol
+++ ether-simple-storage/SimpleStorage.sol
@@ -29,3 +29,14 @@ contract SimpleStorage {
         nameToFavoriteNumber[_name] = _favoriteNumber;
     }
 }
+
+// 0xd9145CCE52D386f254917e481eB44e9943F39138 // every single smart contract have addresses just like our wallets.
+// Anytime we make changes or modify a value in blockchain, including making a new transaction, it happens in transaction.
+// You cant get rid of deploy contract on the blockchain cos it is immutable.
+// Visibility in smart contract can be private, public, internal or extrernal.
+// When we dont give the visibilty to functions or variables the default visibility is internal.
+// Prefixing the parameter with underscore _ is useful for naming convention.
+// Each blockchain has a different way of calculating their gas but the more stuff you do in your function, the more gas it costs.
+// Global scope/local scope.
+// view and pure function when called alone dont spend gas. Remember we only spend gas if we modify state.
+
