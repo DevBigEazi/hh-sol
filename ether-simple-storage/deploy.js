956feac055be4111a91ad0e6a2150f195da4e8cb
devbigeazi <adesholatajudeen1@gmail.com>
2024-02-06 09:09:13 +0100

    Checking the different btw transaction receipt and transaction deployed

--- ether-simple-storage/deploy.js
+++ ether-simple-storage/deploy.js
@@ -3,29 +3,22 @@ const fs = require("fs-extra");
 
 async function main() {
     // compile solidity in code or compile them separately
-    // http://127.0.0.1:7545
-    const provider = new ethers.JsonRpcProvider("http://127.0.0.1:7545");
+    // http//172.18.64.1:7545
 
-    try {
-        await provider.getBlockNumber();
-    } catch (error) {
-        console.error("Error connecting to the provider. Make sure your local blockchain node is running.")
-        process.exit(1);
-    }
-
-    const wallet = new ethers.Wallet("0x8a79b885ca79310ba787547f778371ee5ae7601cfe5356d1becb4b8c18afd3dc", provider);
+    const provider = new ethers.JsonRpcProvider("http://172.18.64.1:7545");
+    const wallet = new ethers.Wallet("0x016b7a9f0ef7293991bf0c1b5a03e40107a0909f2757546b200b755bfd2db6b7", provider);
     const abi = fs.readFileSync("./SimpleStorage_sol_SimpleStorage.abi", "utf8");
     const binary = fs.readFileSync("./SimpleStorage_sol_SimpleStorage.bin", "utf8");
     const contractFactory = new ethers.ContractFactory(abi, binary, wallet);
     console.log("Deploying, pls wait...");
 
-    try {
-        const contract = await contractFactory.deploy(); // STOP here! wait for contract to deploy...
-        console.log("Contract deployed", contract);
-    } catch (error) {
-        console.log("Error deploying the contract", error)
-        process.exit(1);
-    }
+    const contract = await contractFactory.deploy(); // STOP here! wait for contract to deploy...
+    console.log("Contract deployed", contract);
+    const transactionReceipt = await contract.deploymentTransaction().wait(1);
+    console.log("Here is the transaction deployed:");
+    console.log(contract.deploymentTransaction())
+    console.log("Here is the transaction receipt:");
+    console.log(transactionReceipt);
 }
 
 main()
